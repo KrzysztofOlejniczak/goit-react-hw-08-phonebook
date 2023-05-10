1eaf128c45c76d574af04196e93b79e68bae8aae
Krzysztof Olejniczak <krzysztof.olejniczak@gmail.com>
2023-05-10 22:00:41 +0200

    test

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,23 +1,35 @@
 import { Link, Typography } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+
+const logged = true;
 
 export const HomePage = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <Typography variant="h4" sx={{ mx: 'auto', mt: 8, textAlign: 'center' }}>
-        Welcome to the PhoneBook
-      </Typography>
-      <Typography variant="h6" sx={{ mx: 'auto' }}>
-        Please{' '}
-        <Link component={RouterLink} to={'/login'}>
-          log in
-        </Link>{' '}
-        or{' '}
-        <Link component={RouterLink} to={'/signup'}>
-          register
-        </Link>
-        !
-      </Typography>
+      {logged ? (
+        navigate('/contacts', { replace: true })
+      ) : (
+        <>
+          <Typography
+            variant="h4"
+            sx={{ mx: 'auto', mt: 8, textAlign: 'center' }}
+          >
+            Welcome to the PhoneBook
+          </Typography>
+          <Typography variant="h6" sx={{ mx: 'auto' }}>
+            Please{' '}
+            <Link component={RouterLink} to={'/login'}>
+              log in
+            </Link>{' '}
+            or{' '}
+            <Link component={RouterLink} to={'/signup'}>
+              register
+            </Link>
+            !
+          </Typography>
+        </>
+      )}
     </>
   );
 };
